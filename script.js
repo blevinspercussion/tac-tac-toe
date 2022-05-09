5a539f19bddf8929dcbd11b6eae582ec5fcec934
blevinspercussion <blevinspercussion@gmail.com>
2022-05-09 10:34:10 -0500

    placeholder html and css in place

---
+++ script.js
@@ -0,0 +1,40 @@
+// Player factory
+const player = (marker, playerNo) => {
+
+
+    return {marker, playerNo};
+};
+
+
+//Gameboard module
+const gameBoard = (() => {
+    let gameBoardArray = [null, null, null, 
+                          null, null, null,
+                          null, null, null]
+
+    // Need function to handle player moves
+
+
+    // Export variables and functions
+    return { gameBoardArray };
+});
+
+
+
+//Display controller module
+const displayController = (() => {
+    // function to draw the board
+    const drawBoard = () => {
+
+    }
+
+    // Need function to update the board after player move
+
+    // Export variables and functions
+});
+
+
+
+///////////////////////////////////
+//// Event handlers
+///////////////////////////////////
